e8053d84285753ceefe085ea0269f76926554862
sbalasa <santhosh.kbr@gmail.com>
2021-06-27 03:11:13 +0000

    Finding sum for various types of inputs

--- algorithm.py
+++ algorithm.py
@@ -1,13 +1,23 @@
 #!/bin/python3
 
 
-def add(x, y):
-    return x + y
-
-
-def main():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+class StringCalulator:
+    def sum(self, num=None):
+        try:
+            _delimiter = ","
+            if num.startswith("//"):
+                _delimiter = num[2]
+                num = num[3:]
+            negatives = [i for i in num.split(_delimiter) if "-" in i]
+            if negatives:
+                return f"negatives not allowed: {' '.join(negatives)}"
+            _thousands = [i for i in num.split(_delimiter) if int(i) > 1000]
+            if _thousands:
+                num = [i for i in num.split(_delimiter) if int(i) < 1000]
+            if num:
+                _split = "".join(num).split(_delimiter)
+                return sum(map(int, _split))
+            else:
+                return 0
+        except AttributeError:
+            return 0
--- test_algorithm.py
+++ test_algorithm.py
@@ -1,11 +1,55 @@
 import pytest
 
-from algorithm import add
+from algorithm import StringCalulator
+
+# Global
+sc_obj = StringCalulator()
 
 
-def test_add_correct():
-    assert 7 == add(4, 3)
+def test_sum_empty():
+    assert 0 == sc_obj.sum()
 
 
-def test_add_incorrect():
-    assert 3 != add(4, 3)
+def test_same_num():
+    num = "3"
+    assert 3 == sc_obj.sum(num)
+
+
+def test_two_num():
+    num = "1,2"
+    assert 3 == sc_obj.sum(num)
+
+
+def test_three_sum():
+    num = "1,2,3"
+    assert 6 == sc_obj.sum(num)
+
+
+def test_new_line_sum():
+    num = "1\n,2,3"
+    assert 6 == sc_obj.sum(num)
+
+
+def test_custom_delimiter_sum():
+    num = "//;\n1;2"
+    assert 3 == sc_obj.sum(num)
+
+
+def test_single_negative_sum():
+    num = "-5"
+    assert "negatives not allowed: -5" == sc_obj.sum(num)
+
+
+def test_multiple_negative_sum():
+    num = "4,-5,6,-7"
+    assert "negatives not allowed: -5 -7" == sc_obj.sum(num)
+
+
+def test_single_thousands_sum():
+    num = "1001"
+    assert 0 == sc_obj.sum(num)
+
+
+def test_multiple_thousands_sum():
+    num = "2,1001"
+    assert 2 == sc_obj.sum(num)
